7d79f7236e3c85c27a56be484b67de0884438e59
op8mnl <zheng.felix@outlook.com>
2022-10-13 14:38:26 -0400

    message

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
-const express = require('express')
+const { response } = require('express');
+const express = require('express');
 const app = express();
+const api_url = "https://arxiv.org/abs/1612.01840";
 
+app.use(express.json());
+
+async function getData(api_url) {
+    const res = await fetch(api_url)
+    var data = await response.json()
+}
+
+app.get('/', (req, res) => {
+    res.send('Hello World')
+});
+
+//Assigning Port
+const port = process.env.PORT || 3000
+app.listen(port,() => { console.log(`listening on port ${port}...`)});
