1737b4937bb8afbcc13e62b601cc60eedbcb9289
op8mnl <zheng.felix@outlook.com>
2022-10-15 18:35:23 -0400

    backend updates

--- index.js
+++ index.js
@@ -1,16 +1,33 @@
 const express = require('express');
 const app = express();
-const https = require('https');
-const api_url = "https://arxiv.org/abs/1612.01840";
+//Assigning Port
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-https.get()
+const tracks = [
+    {genre_id: 1,noTracks:8693,parent_id:38,genre: 'Avant-Garde',topLevel:38},
+];
 
-app.get('/', (req, res) => {
-    res.send(getData(api_url))
+
+//using express to load front end files from static
+app.use('/',express.static('static')); 
+
+//routing for all tracks
+app.get('/api/tracks/', (req,res) =>{
+    console.log(`GET response for ${req.url}`)
+    res.send(tracks)
+});
+//routing for specific tracks using parameter
+app.get('/api/tracks/:track_id', (req,res) =>{
+    const id = req.params.track_id;
+    console.log(`GET response for ${req.url}`)
+    const track = tracks.find(t => t.id === parseInt(id));
+    if (track){ 
+        res.send(track)
+    }else{
+        res.status(404).send(`Part ${id} was not found`);
+    }
 });
 
-//Assigning Port
-const port = process.env.PORT || 3000
 app.listen(port,() => { console.log(`listening on port ${port}...`)});
